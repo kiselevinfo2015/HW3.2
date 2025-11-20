cf054e140d6e9737233469bee41ecba1a83a43e2
kiselevinfo2015 <kiselev.info2015@yandex.ru>
2025-11-20 21:37:14 +0300

    Упорядочить числа.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main() {
+    int a, b, c;
+    
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе число: ";
+    std::cin >> b;
+    std::cout << "Введите третье число: ";
+    std::cin >> c;
+    
+    int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    
+    int min = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+    
+    int mid = (a != max && a != min) ? a : ((b != max && b != min) ? b : c);
+    
+    std::cout << "Результат: " << max << " " << mid << " " << min << std::endl;
+    
+    return 0;
+}
